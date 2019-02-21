0b5ae6c17de4913408eea6b1a368b100c8cb9fa9
logeeqw <43319952+logeeqw@users.noreply.github.com>
2019-02-20 23:41:43 -0500

    Add files via upload

--- public/movies.js
+++ public/movies.js
@@ -6,11 +6,11 @@
  */
 
 const PROMPT = require(`readline-sync`);
-let movieTitle;
-let numRatings, avgRating, totalRating, rateMovie = [], movies = [], chooseMovie;
+let numRatings, avgRating, totalRating, rateMovie = [], movies = [], chooseMovie, rating;
 let rankMovie = [];
 let continueResponse;
-const MAX_RATING = 10, MIN_RATING = 0;
+let i = 0;
+const MAX_RATING = 10, MIN_RATING = 0, RATING_INCREMENT = 1;
 
 
 /**
@@ -23,6 +23,9 @@ function main() {
     setContinueResponse();
     while (continueResponse === 1) {
         setChooseMovie();
+      /*  setHappyDeathDay();
+        setColdPursuit();
+        setTheUpside();*/
         // setRateMovie();
         // setTitle();
         setContinueResponse();
@@ -49,48 +52,73 @@ function setContinueResponse() {
 }
 
 function setChooseMovie() {
-    // if (chooseMovie === 1 || chooseMovie === 2 || chooseMovie === 3 || chooseMovie === 4 || chooseMovie === 5){
-    chooseMovie = Number(PROMPT.question(`\nWhat would you like to do? \n1. Rate: Happy Death Day 2U
-            \n2. Rate: Cold Pursuit \n3. Rate: The Upside \n4. Choose a new movie to rate \n 5. Rank movies`));
-    while (chooseMovie = 1){
-setHappyDeathDay();
-   // break;}
-    while (chooseMovie = 2){
-        setColdPursuit();
-    // break;}
-    while (chooseMovie = 3){
-       setTheUpside();
-    // break;}
-    /*while (chooseMovie = 4)
-        movieTitle = PROMPT.question(`Please enter a movie title: `);
-        PROMPT.question(`Please enter a rating for this movie (1-10): `);
-    if ( chooseMovie <MIN_RATING || chooseMovie> MAX_RATING) {
-        console.log(`${chooseMovie} is invalid. Please try again.`);
-    }*/
+    for (/*let i = 0;*/ i < numRatings; i++;)
+        chooseMovie[i] = [];
+    while (chooseMovie !== null) {
+        chooseMovie = Number(PROMPT.question(`\nWhat would you like to do? \n1. Rate: Happy Death Day 2U
+2. Rate: Cold Pursuit \n3. Rate: The Upside \n4. Choose a new movie to rate \n5. Rank movies \n`));
+        if (chooseMovie = 1) {
+            chooseMovie[i][0] = PROMPT.question(`Please enter a rating for Happy Death Day 2U (1-10): `);
+            if (chooseMovie < MIN_RATING || chooseMovie > MAX_RATING) {
+                console.log(`${rating} is invalid. Please try again.`);
+            }
+            while (chooseMovie[i][0] < MAX_RATING && chooseMovie[i][0] > MIN_RATING) {
+                chooseMovie[i][1] = chooseMovie[i][0];
+                chooseMovie[i][1] = RATING_INCREMENT + numRatings;
+            }
+            break;
+        }
+    }
+}
 
-// }
-}}
+
+
+         if (chooseMovie = 2) {
+            setColdPursuit();
+        }
+        else if (chooseMovie = 3) {
+            setTheUpside();
+        }
+        else if (chooseMovie = 4) {
+            setNewMovie();
+        }
+
+
 
 function setHappyDeathDay() {
-    PROMPT.question(`Please enter a rating for Happy Death Day 2U (1-10): `);
+    rating = PROMPT.question(`Please enter a rating for Happy Death Day 2U (1-10): `);
     if (chooseMovie < MIN_RATING || chooseMovie > MAX_RATING) {
-        console.log(`${chooseMovie} is invalid. Please try again.`);
+        console.log(`${rating} is invalid. Please try again.`);
     }
 }
 
 function setColdPursuit() {
-    PROMPT.question(`Please enter a rating for Cold Pursuit (1-10): `);
+    console.log(`work please`);
+    rating = PROMPT.question(`Please enter a rating for Cold Pursuit (1-10): `);
     if (chooseMovie < MIN_RATING || chooseMovie > MAX_RATING) {
-        console.log(`${chooseMovie} is invalid. Please try again.`);
+        console.log(`${rating} is invalid. Please try again.`);
     }
 }
 
 function setTheUpside() {
-    PROMPT.question(`Please enter a rating for The Upside (1-10): `);
+    rating = PROMPT.question(`Please enter a rating for The Upside (1-10): `);
     if (chooseMovie < MIN_RATING || chooseMovie > MAX_RATING) {
-        console.log(`${chooseMovie} is invalid. Please try again.`);
+        console.log(`${rating} is invalid. Please try again.`);
     }
-}}}
+}
+
+function setNewMovie() {
+    let movieTitle;
+    movieTitle = PROMPT.question(`Please enter a movie title: `);
+    rating = PROMPT.question(`Please enter a rating for ${movieTitle} (1-10): `);
+    if ( chooseMovie <MIN_RATING || chooseMovie> MAX_RATING) {
+        console.log(`${rating} is invalid. Please try again.`);
+    }
+}
+
+function setAvgRating() {
+    avgRating = rating * numRatings / numRatings
+}
 
 /**
  * @method
@@ -138,9 +166,7 @@ function setTheUpside() {
         }*!/
 }}*/
 
-/*function setTitle() {
-    movieTitle = PROMPT.question(`Please enter movie title: `);
-}
+/*
 
 function setNum() {
  console.log(`Penis`)
